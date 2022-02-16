a288b4f8ab9e2e9946f43e96a8557f771c74b0d7
MRN626 <ngalukiyemaureen@gmail.com>
2022-02-16 09:36:51 +0200

    creation modele de classe

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,32 @@
 
 namespace App\Controllers;
 
+//use App\Models\Chef_departement_model;
+
 class Home extends BaseController
 {
     public function index()
     {
+        
+
+        
+        /**CRUD TEST
+         $model_chef_departement = new Chef_departement_model();
+        AJOUT avec insert
+        $donnees = ['NOM_COMPLET'=>"ngalukiye maureen",'SIGNATURE_CHEF_DEP'=>"MRN"];
+        $model_chef_departement ->insert($donnees);
+
+        LECTURE avec find all
+        $departement = $model_chef_departement ->findAll();
+        var_dump($departement);
+
+        MODIFICATION avec update
+        $donnees =['NOM_COMPLET'=>"CHARMANT SAMPI"];
+        $model_chef_departement->update(2,$donnees);
+
+        SUPPRESSION avec delete
+        $model_chef_departement->delete(1);**/
+
         return view('pages/dashboard');
     }
 }
